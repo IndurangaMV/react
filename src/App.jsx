c15ec9233084f0835d3e20f7ccd2c29ee720cbe3
induranga <induranga21297@gmail.com>
2025-02-21 13:55:36 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; // Optional Bootstrap styling
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap styling
 import { Container } from "react-bootstrap";
-import Flower from "./Flower";
-import Rabbit from "./Rabbit";
-import Football from "./Football";
 
-function MissGoal(){
-  return <h1>Missed..!</h1>
+function Car(props) {
+  return <li>Hai, I am {props.brand} </li>;
 }
-function MadeGoal(){
-  return <h1>Goal..!</h1>
+
+function Garage() {
+  const car = [
+    { id: 1, brand: 'Ford' },
+    { id: 2, brand: 'BMW' },
+    { id: 3, brand: 'Toyota' },
+    { id: 4, brand: 'Audi' }
+  ];
+
+  return (
+    <>
+      <h2>Garage</h2>
+      {car.length > 0 && <h3>You have {car.length} cars in the garage.</h3>}
+      <ul>
+        {car.map((cars) => <Car key={cars.id} brand={cars.brand} />)} 
+      </ul>
+    </>
+  );
 }
-function Choose(props){
-  if(!props.isGoal){
-    return <MissGoal/>
-  }else{
-    return <MadeGoal/>
-  }
+
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(inputs));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input type="text" name="username" value={inputs.username || ""} onChange={handleChange} />
+      </label>
+      <br />
+      <label>Enter your age:
+        <input type="number" name="age" value={inputs.age || ""} onChange={handleChange} />
+      </label>
+      <br />
+      <input type="submit" className="btn btn-primary mt-3" />
+    </form>
+  );
 }
 
 function App() {
   return (
     <Container className="mt-5">
-     
-      <Choose isGoal={false}/>
+      <MyForm />
+      <Garage />
     </Container>
   );
 }
